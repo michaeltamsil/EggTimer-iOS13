cae102b9ad0228398fbaae85abff0c3f5664f7d4
michael tamsil <michaeltamsil@gmail.com>
2022-08-12 07:04:07 +0700

    learn nil value

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -13,18 +13,11 @@ class ViewController: UIViewController {
     let softTime = 5
     let mediumTime = 7
     let hardTime = 12
+    
+    let eggTimes = ["Soft": 5, "Medium": 7, "Hard": 12]
 
     @IBAction func hardnessSelected(_ sender: UIButton) {
-        let hardness = sender.currentTitle
-        switch hardness {
-            case "Soft":
-                print(softTime)
-            case "Medium":
-                print(mediumTime)
-            case "Hard":
-                print(hardTime)
-            default:
-                print("Error")
-        }
+        let hardness = sender.currentTitle!
+        print(eggTimes[hardness]!)
     }
 }
---
+++ String.playground/Contents.swift
@@ -0,0 +1,9 @@
+var player1Username: String? = nil
+
+player1Username = "jack"
+print(player1Username!)
+
+player1Username = nil
+if player1Username != nil {
+    print(player1Username)
+}
